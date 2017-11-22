cfc975fe69244bae420c259dfa7f99466f4bccc4
Denzel Ooms <DenzelOoms@live.be>
2017-11-22 15:35:46 +0100

    ok

--- GIP Programmeren/GIP Programmeren/Leerling.cs
+++ GIP Programmeren/GIP Programmeren/Leerling.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace GIP_Programmeren
 {
@@ -12,6 +14,11 @@ namespace GIP_Programmeren
         private int _klasnummer;
         private string _voornaam;
         private string _achternaam;
+        private string _password;
+        private bool _maandag;
+        private bool _dinsdag;
+        private bool _donderdag;
+        private bool _vrijdag;
 
         public string strIdnummer
         {
@@ -37,8 +44,50 @@ namespace GIP_Programmeren
             set { _achternaam = value; }
         }
 
+        public string strPassword
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public bool blMaandag
+        {
+            get { return _maandag; }
+            set { _maandag = value; }
+        }
+
+        public bool blDinsdag
+        {
+            get { return _dinsdag; }
+            set { _dinsdag = value; }
+        }
+
+        public bool bldonderdag
+        {
+            get { return _donderdag; }
+            set { _donderdag = value; }
+        }
+
+        public bool blvrijdag
+        {
+            get { return _vrijdag; }
+            set { _vrijdag = value; }
+        }
+
+
+
+
+
+
         public void VoegLeerlingToeAanDB()
         {
+            string _conn = string.Format("server=84.196.202.210;user id=Denzel;database=arduino;password={0}", strPassword);
+            MySqlConnection conn = new MySqlConnection(_conn);
+            string _cmd = string.Format("INSERT INTO Leerling ('', '', '', '')",)
+            MySqlCommand cmd = 
+
+
+
 
         }
 
@@ -47,6 +96,10 @@ namespace GIP_Programmeren
 
         }
 
+        public Leerling(string _strPW)
+        {
+            strPassword = _strPW;
+        }
 
 
     }
