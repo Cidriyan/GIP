225e7289b2942ef3d3e05d8ab06fe5ca9a050711
Olivier Fuenzalida <OlivierFP@hotmail.com>
2017-11-22 11:27:08 +0100

    Class Klas

---
+++ GIP Programmeren/GIP Programmeren/Klas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIP_Programmeren
+{
+    class Klas
+    {
+        private DateTime _tijdstip;
+        private String _richting;
+        private int _jaar;
+        private List<Leerling> = new List<Leerling>();
+
+        public DateTime dtTijdstip
+        {
+            get { return _tijdstip; }
+            set { _tijdstip = value; }
+        }
+
+        public String strRichting
+        {
+            get { return _richting; }
+            set { _richting = value; }
+        }
+
+        public int intJaar
+        {
+            get { return _jaar; }
+            set { _jaar = value; }
+        }
+        
+
+
+    }
+}
